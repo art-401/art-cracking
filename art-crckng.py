e4180bbd142d0610f42d118ab4a2cf9a49145de3
art-401 <157432095+art-401@users.noreply.github.com>
2024-01-24 10:52:11 +0800

    Update art-crckng.py

--- art-crckng.py
+++ art-crckng.py
@@ -21,17 +21,17 @@ ugen=[]
 ugen=[]
 useragent=[]
 for xd in range(10000):
-         aa='Mozilla/5.0 (Linux; U; Android' 
-	 b=random.choice(['6','7','8','9','10','11','12','13'])
-	 c=f' TL-tl; {str(gt)}'
-	 g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
-	 h=random.randrange(73,100)
-	 i='0'
-	 j=random.randrange(4200,4900)
-	 k=random.randrange(40,150)
-	 l='Mobile Safari/537.36'
-	 uaku2=f'{aa} {b}; {c}) {g}{h}.{i}.{j}.{k} {l}'
-	 ugen.append(uaku2)
+         aa='Mozilla/5.0 (Linux; U; Android'
+        b=random.choice(['6','7','8','9','10','11','12','13'])
+        c=f' TL-tl; {str(gt)}'
+        g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+        h=random.randrange(73,100)
+        i='0'
+        j=random.randrange(4200,4900)
+        k=random.randrange(40,150)
+        l='Mobile Safari/537.36'
+        uaku2=f'{aa} {b}; {c}) {g}{h}.{i}.{j}.{k} {l}'
+        ugen.append(uaku2)
 for agent in range(10000):
         aa='Mozilla/5.0 (Linux; Android 6.0.1;'
         b=random.choice(['6','7','8','9','10','11','12'])
